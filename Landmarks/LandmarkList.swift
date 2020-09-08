6486b866fd6034c672dc11827929aac350436fb5
technosoul <rockdrigo.mtz@gmail.com>
2020-09-07 21:18:35 -0500

    section 5 completed :boom:

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -8,17 +8,17 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
-    @State var showFavoritesOnly = true
+    @EnvironmentObject var userData: UserData
     
     var body: some View {
         NavigationView {
             List{
-                Toggle(isOn: $showFavoritesOnly){
+                Toggle(isOn: $userData.showFavoritesOnly) {
                     Text("Favorites only")
                 }
                 
                 ForEach(landmarkData) { landmark in
-                    if !self.showFavoritesOnly || landmark.isFavorite {
+                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
                         NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                                 LandmarkRow(landmark: landmark)
                         }
@@ -34,6 +34,7 @@ struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
             LandmarkList()
+                .environmentObject(UserData())
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
         }
